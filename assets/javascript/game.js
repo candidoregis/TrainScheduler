f9373e7187379f5888d1cc69b1c06c4b468a86a0
Candido Regis <candidorbb@gmail.com>
2019-07-26 14:48:25 -0400

    updating css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+// 
 const firebaseConfig = {
     apiKey: "AIzaSyDK9MMQvhGU2SF-ou8LYFgljKTgIjCEBvc",
     authDomain: "trainscheduler7-1.firebaseapp.com",
@@ -8,81 +9,38 @@ const firebaseConfig = {
     appId: "1:183019342927:web:ea14bd05e261a570"
 };
 
+//
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
-
-
-var randomDate = "02/23/1999";
-var randomFormat = "MM/DD/YYYY";
-var convertedDate = moment(randomDate, randomFormat);
-// console.log(convertedDate);
-
-
-
-
-// All of our connections will be stored in this directory.
-// var connectionsRef = database.ref("/connections");
-
-// '.info/connected' is a special location provided by Firebase that is updated
-// every time the client's connection state changes.
-// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
-// var connectedRef = database.ref(".info/connected");
-
-// connectedRef.on("value", function (snap) {
-
-//     // If they are connected..
-//     if (snap.val()) {
-
-//         // Add user to the connections list.
-//         var con = connectionsRef.push(true);
-//         // Remove user from the connection list when they disconnect.
-//         con.onDisconnect().remove();
-//     }
-// });
-
-// When first loaded or when the connections list changes...
-// connectionsRef.on("value", function (snap) {
-
-//     // Display the viewer count in the html.
-//     // The number of online users is the number of children in the connections list.
-//     $("#connected-viewers").text(snap.numChildren());
-// });
-
-// -----
-
+//
 function validateTrain(name, dest, ftime, freq) {
     var isValid = ((name === "") || (dest === "") || (ftime === "") || (freq === ""));
-    console.log("is valid: " + isValid);
     return isValid;
-    // } else {
-    // return false;
-    // }
 }
 
+// 
 function minAwayCalc(fitime, freq) {
     var firstTimeConverted = moment(fitime, "HH:mm").subtract(1, "years");
-    var currentTime = moment();
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
     var tRemainder = diffTime % freq;
     var tMinutesTillTrain = freq - tRemainder;
     return tMinutesTillTrain;
 }
 
+// 
 function nextTrainCalc(minutes) {
     var nextTrain = moment().add(minutes, "minutes");
     return moment(nextTrain).format("hh:mm");
 }
 
+// 
 function timeLengthValidation(inputField) {
-    console.log("length: " + inputField.length);
-    if (inputField.length == 4) {
-        return true;
-    } else {
-        return false;
-    }
+    var isValid = (inputField.length == 4);
+        return isValid;
 }
 
+// 
 function timeValidation(inputField) {
     if (inputField == "2400") {
         inputField = 0000;
@@ -91,16 +49,20 @@ function timeValidation(inputField) {
     return isTimeValid;
 }
 
+// 
 function frequencyValidation(inputField) {
     var isFreqValid = /^[0-9]+$/.test(inputField);
     return isFreqValid;
 }
 
+//
 function retrieveData() {
     $("#trainSchedules").empty();
     var query = firebase.database().ref();
     query.once("value")
         .then(function (snapshot) {
+
+            // Go through all children in DB and retrieving data
             snapshot.forEach(function (childSnapshot) {
                 var key = childSnapshot.key;
                 var childData = childSnapshot.val();
@@ -109,6 +71,7 @@ function retrieveData() {
         });
 }
 
+// 
 function addData(data) {
     var name = data.trainName;
     var destination = data.trainDestination;
@@ -137,10 +100,9 @@ $("#addNewTrain").on("click", function () {
     var trainName = $("#inlineFormInputName").val().trim();
     var trainDestination = $("#inlineFormInputDestination").val().trim();
     var trainFirstTime = $("#inlineFormInputTrainTime").val().trim();
-
     var trainFrequency = $("#inlineFormInputFrequency").val().trim(); 
 
-    // Validate entries
+    // Validate entries, if good add to DB
     if (validateTrain(trainName, trainDestination, trainFirstTime, trainFrequency)) {
         $('#exampleModal').modal('show');
     } else if ((timeLengthValidation(trainFirstTime))
